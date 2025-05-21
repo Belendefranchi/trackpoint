196aace413deff6b07354696e603e06355e76e4e
Maria Belén De Franchi <belendefranchi@gmail.com>
2025-05-21 18:06:40 -0300

    Renombro menu.configuracion.DataTables.js a menu.configuracion.js y actualizo referencias

--- app/modules/configuracion/views/abm.perfilesPorOperador.view.php
+++ app/modules/configuracion/views/abm.perfilesPorOperador.view.php
@@ -167,7 +167,7 @@ require_once __DIR__ . '/../../../../core/permisos.php';
 	<?php require_once __DIR__ . '/../../../layouts/layout.scripts.php'; ?>
 
   <!-- Script DataTables y modales -->
-  <script src="/trackpoint/public/assets/js/menu_configuracion/menu.configuracion.DataTables.js"></script>
+  <script src="/trackpoint/public/assets/js/menu_configuracion/menu.configuracion.js"></script>
   <script src="/trackpoint/public/assets/js/menu_configuracion/abm.perfilesPorOperador.modales.js"></script>
 
 </body>
